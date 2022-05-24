63ec06eb5c093aa292c866ef7813596546d64cc4
JVRasa <j.rasamoelina@gmail.com>
2022-05-24 16:50:37 +0200

    added search bar

--- components/r_product.js
+++ components/r_product.js
@@ -1,9 +1,22 @@
 import styles from "../styles/Reservation.module.css";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function ResProduct({ product }) {
+  const [addedCart, setAddedCart] = useState(false);
+
+  const handleClickProduct = () => {
+    setAddedCart(!addedCart);
+  };
+
   return (
-    <div className={styles.productWrapper}>
+    <div
+      className={styles.productWrapper}
+      onClick={handleClickProduct}
+      style={
+        addedCart ? { borderColor: "#96C0C0" } : { borderColor: "#ededed" }
+      }
+    >
       <Image
         src="/image/r_baby-stroller.png"
         height={70}
--- pages/reservation.js
+++ pages/reservation.js
@@ -1,5 +1,6 @@
 import ResProduct from "../components/r_product";
 import styles from "../styles/Reservation.module.css";
+import { useState } from "react";
 
 const productsList = [
   {
@@ -28,18 +29,36 @@ const productsList = [
   },
   {
     id: 5,
-    name: "Poussette",
-    category: "poussette",
+    name: "Lit à barreaux",
+    category: "lit_barreaux",
     price: 12,
   },
 ];
 
 export default function ReservationPage() {
+  const [searchValue, setSearchValue] = useState("");
+
   return (
     <div className={styles.mainWrapper}>
-      {productsList.map((product) => (
-        <ResProduct key={product.id} product={product} />
-      ))}
+      <section className={styles.searchContainer}>
+        <input
+          value={searchValue}
+          type="text"
+          placeholder="Poussette, lit à barreaux, chaise haute..."
+          onChange={(event) => setSearchValue(event.target.value)}
+          className={styles.searchBar}
+        />
+      </section>
+      <section className={styles.productNotAvailable}>
+        <input type="checkbox" />
+        Afficher les produits indisponibles
+      </section>
+
+      {productsList
+        .filter((product) => product.category.includes(searchValue))
+        .map((product) => (
+          <ResProduct product={product} key={product.id} id={product.id} />
+        ))}
     </div>
   );
 }
