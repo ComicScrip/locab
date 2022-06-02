b60e01ddc9b95874a38e1fa886c07258a2f2c9ef
Claiirou <clairemachet@hotmail.fr>
2022-06-02 12:13:55 +0200

    finish sign up page

--- pages/signup/index.js
+++ pages/signup/index.js
@@ -2,6 +2,9 @@ import { useState } from "react";
 
 export default function SignUpPage() {
   const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirmation, setPasswordConfirmation] = useState("");
   return (
     <>
       <h1>Sign Up</h1>
@@ -12,6 +15,30 @@ export default function SignUpPage() {
             id="name"
             value={name}
             onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+        <label htmlFor="email">
+          <input
+            type="text"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label htmlFor="password">
+          <input
+            type="text"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <label htmlFor="passwordconfirmation">
+          <input
+            type="text"
+            id="passwordconfirmation"
+            value={passwordConfirmation}
+            onChange={(e) => setPasswordConfirmation(e.target.value)}
           />
         </label>
       </form>
