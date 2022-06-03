09523b954284eb16a09376ab4060f2b551b6d515
dialloth <althierno@yahoo.fr>
2022-06-03 14:20:24 +0200

    test

--- cypress/integration/footer.spec.js
+++ cypress/integration/footer.spec.js
@@ -1,9 +1,11 @@
+/* eslint-disable prettier/prettier */
 describe("footer", () => {
   beforeEach(() => {
     cy.visit("/");
   });
 
-  it("displays a title", () => {
-    cy.get("h1").should("not.be.empty");
+  it("displays links, img", () => {
+    cy.get("a").should("not.be.empty");
+    cy.get("img").should("not.be.empty");
   });
 });
