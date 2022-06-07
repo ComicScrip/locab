74b491bd42a1fea1729ec69f9599a4ec478238c9
dialloth <althierno@yahoo.fr>
2022-06-07 19:57:05 +0200

    menu burger

--- components/header.js
+++ components/header.js
@@ -3,18 +3,30 @@
 /* eslint-disable prettier/prettier */
 import React from "react";
 import Link from "next/link";
+import { useState } from "react";
 import Image from "next/image";
 import Logo from "../public/logo/logo.png";
 import LogoText from "../public/logo/logoText.png";
+import LogoTransparent from "../public/logo/logo_transparent.png";
+import LogoIcon from "../public/logo/icon_logo.png";
 import styles from "../styles/headerfooter/navbar.module.css";
 
 const Navbar = () => {
+  const [showLinks, setshowLinks] = useState(false);
+
+  const handleShowLinks = () => {
+    setshowLinks(!showLinks);
+  };
   return (
     <nav className={styles.navbar}>
       <div className={styles.logocontainer}>
         <Link href="/" className={styles.logo}>
           <a>
-            <Image src={Logo} className={styles.logo_img} alt="logo" />
+            <Image
+              src={Logo}
+              className={styles.logo_img}
+              alt="logo_principal"
+            />
           </a>
         </Link>
         <Link href="/" className={styles.logo}>
@@ -38,14 +50,39 @@ const Navbar = () => {
         </Link>
         <Link href="reservation">
           <a>
-            {" "}
-            <button className={styles.reservationButton}>
-              {" "}
-              Réservation{" "}
-            </button>{" "}
+            <button className={styles.reservationButton}>Réservation</button>
           </a>
         </Link>
       </ul>
+      <div className={styles.divlogoIcon1}>
+        <Link href="/" className={styles.linklogotransparent}>
+          <a>
+            <Image
+              src={LogoTransparent}
+              className={styles.logo_transparent}
+              alt="logoTransparent"
+            />
+          </a>
+        </Link>
+      </div>
+      <div className={styles.divlogoIcon}>
+        <Link href="/">
+          <a>
+            <Image
+              src={LogoIcon}
+              className={styles.logo_icon}
+              alt="logo_icon"
+            />
+          </a>
+        </Link>
+      </div>
+      <button
+        type="button"
+        className={styles.btnBurger}
+        onClick={handleShowLinks}
+      >
+        <span className="Burger_Line towLine" />
+      </button>
     </nav>
   );
 };
