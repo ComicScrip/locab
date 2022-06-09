f5c8fb9d0897ae7645ac3fbf719f9a8d0ac5d543
RayaneArroudj <rayane.arroudj@gmail.com>
2022-06-09 17:29:00 +0200

    page recap

--- pages/recap/index.js
+++ pages/recap/index.js
@@ -1,5 +1,8 @@
 import Head from "next/head";
 import { GiPadlock } from "react-icons/gi";
+import { BsPaypal } from "react-icons/bs";
+import { FaCcVisa } from "react-icons/fa";
+import { FaCcMastercard } from "react-icons/fa";
 import styles from "../../styles/Welcome.module.css";
 /* import Welcome from "../../components/recap/welcome"; */
 /* import Informations from "../..//components/recap/Informations"; */
@@ -7,13 +10,70 @@ import styles from "../../styles/Welcome.module.css";
 import { useState } from "react";
 
 export default function Recap() {
-  const [active, setActive] = useState(false);
-  const [close, setClose] = useState(true);
+  /* PARTIE WELCOME */
 
-  const HandleSubmit = (e) => {
+  const [showWelcome, setShowWelcome] = useState(true);
+  const [activeInformations, setActiveInformations] = useState(false);
+  const [activeLivraison, setActiveLivraison] = useState(false);
+  const [activePayment, setActivePayment] = useState(false);
+
+  /* PARTIE BIENVENUE */
+
+  const HandleSubmitWelcome = (e) => {
     e.preventDefault();
-    setActive(!active);
-    setClose(!close);
+    setShowWelcome(!showWelcome);
+    setActiveInformations(!activeInformations);
+  };
+
+  const OpenWelcome = (e) => {
+    e.preventDefault();
+    setShowWelcome(!showWelcome);
+  };
+
+  /* PARTIE INFORMATIONS */
+
+  const HandleSubmitInformations = (e) => {
+    e.preventDefault();
+    setActiveInformations(!activeInformations);
+    setActiveLivraison(!activeLivraison);
+  };
+
+  const OpenInformations = (e) => {
+    e.preventDefault();
+    setActiveInformations(!activeInformations);
+  };
+
+  /* PARTIE LIVRAISON */
+
+  const HandleSubmitLivraison = (e) => {
+    e.preventDefault();
+    setActiveLivraison(!activeLivraison);
+    setActivePayment(!activePayment);
+  };
+
+  const OpenLivraison = (e) => {
+    e.preventDefault();
+    setActiveLivraison(!activeLivraison);
+  };
+
+  // PARTIE PAIEMENT
+
+  const HandleSubmitPayment = (e) => {
+    e.preventDefault();
+    setActivePayment(!activePayment);
+  };
+
+  const OpenPayment = (e) => {
+    e.preventDefault();
+    setActivePayment(!activePayment);
+  };
+
+  const style = {
+    color: "red",
+  };
+
+  const styleblack = {
+    color: "black",
   };
 
   return (
@@ -44,13 +104,15 @@ export default function Recap() {
           <hr className={styles.hr} />
         </div>
         <div className={styles.h2}>
-          <h2>Bienvenue</h2>
+          <h2 onClick={OpenWelcome} style={showWelcome ? styleblack : style}>
+            Bienvenue
+          </h2>
         </div>
         <div className={styles.ligne}>
           <hr className={styles.hr} />
         </div>
       </div>
-      {close && (
+      {showWelcome && (
         <>
           <h3 className={styles.h3}>Le paiement est sécurisé</h3>
           <div className={styles.containerbloc}>
@@ -83,7 +145,7 @@ export default function Recap() {
                   <button
                     type="submit"
                     className={styles.button}
-                    onClick={HandleSubmit}
+                    onClick={HandleSubmitWelcome}
                   >
                     SE CONNECTER
                   </button>
@@ -104,7 +166,11 @@ export default function Recap() {
                     required
                   />
                   <div className={styles.formbutton}>
-                    <button type="submit" className={styles.button}>
+                    <button
+                      type="submit"
+                      className={styles.button}
+                      onClick={HandleSubmitWelcome}
+                    >
                       SE CONNECTER
                     </button>
                   </div>
@@ -119,14 +185,14 @@ export default function Recap() {
           <hr className={styles.hr} />
         </div>
         <div className={styles.h2}>
-          <h2>Informations</h2>
+          <h2 onClick={OpenInformations}>Informations</h2>
         </div>
         <div className={styles.ligne}>
           <hr className={styles.hr} />
         </div>
       </div>
 
-      {active && (
+      {activeInformations && (
         <>
           <h3 className={styles.h3}>
             Indispensable pour que nous puissions traiter votre commande
@@ -138,12 +204,7 @@ export default function Recap() {
                   <label htmlFor="email" className={styles.email}>
                     Adresse mail
                   </label>
-                  <input
-                    className={styles.textarea}
-                    id="email"
-                    type="email"
-                    required
-                  />
+                  <input className={styles.textarea} id="email" type="email" />
                 </div>
                 <div className={styles.name}>
                   <div className={styles.formpassword}>
@@ -154,7 +215,6 @@ export default function Recap() {
                       className={styles.textarea}
                       id="firstname"
                       type="name"
-                      required
                     />
                   </div>
                   <div className={styles.formpassword}>
@@ -165,7 +225,6 @@ export default function Recap() {
                       className={styles.textarea}
                       id="secondname"
                       type="name"
-                      required
                     />
                   </div>
                 </div>
@@ -177,7 +236,6 @@ export default function Recap() {
                     className={styles.textarea}
                     id="adress"
                     type="adress"
-                    required
                   />
                 </div>
                 <div className={styles.name}>
@@ -189,19 +247,13 @@ export default function Recap() {
                       className={styles.textarea}
                       id="codepostal"
                       type="adress"
-                      required
                     />
                   </div>
                   <div className={styles.formpassword}>
                     <label htmlFor="city" className={styles.password}>
                       Ville
                     </label>
-                    <input
-                      className={styles.textarea}
-                      id="city"
-                      type="city"
-                      required
-                    />
+                    <input className={styles.textarea} id="city" type="city" />
                   </div>
                 </div>
                 <div className={styles.checkbox}>
@@ -214,12 +266,11 @@ export default function Recap() {
                     Je souhaite m&apos;inscrire
                   </label>
                 </div>
-
                 <div className={styles.formbutton}>
                   <button
-                    type="submit"
                     className={styles.button2}
-                    onSubmit={HandleSubmit}
+                    type="submit"
+                    onClick={HandleSubmitInformations}
                   >
                     CONTINUER VERS LA LIVRAISON
                   </button>
@@ -235,136 +286,214 @@ export default function Recap() {
             <hr className={styles.hr} />
           </div>
           <div className={styles.h2}>
-            <h2>Livraison</h2>
+            <h2 onClick={OpenLivraison}>Livraison</h2>
           </div>
           <div className={styles.ligne}>
             <hr className={styles.hr} />
           </div>
         </div>
-        <h3 className={styles.h3}>Renseignez-nous sur votre lieu de séjour</h3>
-        <div className={styles.containerbloc}>
-          <div className={styles.bloc1}>
-            <form className={styles.inscrit}>
-              <div className={styles.formemail}>
-                <label htmlFor="partenaire" className={styles.email}>
-                  Partenaire
-                </label>
-                <input
-                  className={styles.textarea}
-                  id="partenaire"
-                  type="text"
-                  required
-                />
+        {activeLivraison && (
+          <>
+            <h3 className={styles.h3}>
+              Renseignez-nous sur votre lieu de séjour
+            </h3>
+            <div className={styles.containerbloc}>
+              <div className={styles.bloc1}>
+                <form className={styles.inscrit}>
+                  <div className={styles.formemail}>
+                    <label htmlFor="partenaire" className={styles.email}>
+                      Partenaire
+                    </label>
+                    <input
+                      className={styles.textarea}
+                      id="partenaire"
+                      type="text"
+                      required
+                    />
+                  </div>
+                  <div className={styles.formpassword}>
+                    <label htmlFor="partenaire" className={styles.password}>
+                      Numéro de téléphone de l&apos;hébergeur
+                    </label>
+                    <input
+                      className={styles.textarea}
+                      id="partenaire"
+                      type="text"
+                      required
+                    />
+                  </div>
+                  <div className={styles.name}>
+                    <div className={styles.formpassword}>
+                      <label htmlFor="name" className={styles.password}>
+                        Prénom de l&apos;hébergeur
+                      </label>
+                      <input
+                        className={styles.textarea}
+                        id="firstname"
+                        type="name"
+                        required
+                      />
+                    </div>
+                    <div className={styles.formpassword}>
+                      <label htmlFor="name" className={styles.password}>
+                        Nom de l&apos;hébergeur
+                      </label>
+                      <input
+                        className={styles.textarea}
+                        id="secondname"
+                        type="name"
+                        required
+                      />
+                    </div>
+                  </div>
+                  <div className={styles.formpassword}>
+                    <label htmlFor="adress" className={styles.password}>
+                      Adresse
+                    </label>
+                    <input
+                      className={styles.textarea}
+                      id="adress"
+                      type="adress"
+                      required
+                    />
+                  </div>
+                  <div className={styles.name}>
+                    <div className={styles.formpassword}>
+                      <label htmlFor="codepostale" className={styles.password}>
+                        Code postal
+                      </label>
+                      <input
+                        className={styles.textarea}
+                        id="codepostal"
+                        type="adress"
+                        required
+                      />
+                    </div>
+                    <div className={styles.formpassword}>
+                      <label htmlFor="city" className={styles.password}>
+                        Ville
+                      </label>
+                      <input
+                        className={styles.textarea}
+                        id="city"
+                        type="city"
+                        required
+                      />
+                    </div>
+                  </div>
+                  <div className={styles.formpassword}>
+                    <label htmlFor="hour" className={styles.password}>
+                      Heure d&apos;arrivée
+                    </label>
+                    <input
+                      className={styles.textarea}
+                      id="hour"
+                      type="hour"
+                      required
+                    />
+                  </div>
+                  <div className={styles.formpassword}>
+                    <label htmlFor="hour" className={styles.password}>
+                      Commentaires
+                    </label>
+                    <input
+                      className={styles.textarea2}
+                      id="commentaire"
+                      type="commentaire"
+                      required
+                    />
+                  </div>
+                  <div className={styles.formbutton}>
+                    <button
+                      type="submit"
+                      className={styles.button2}
+                      onClick={HandleSubmitLivraison}
+                    >
+                      CONTINUER VERS LE PAIEMENT
+                    </button>
+                  </div>
+                </form>
               </div>
-              <div className={styles.formpassword}>
-                <label htmlFor="partenaire" className={styles.password}>
-                  Numéro de téléphone de l&apos;hébergeur
-                </label>
-                <input
-                  className={styles.textarea}
-                  id="partenaire"
-                  type="text"
-                  required
-                />
-              </div>
-              <div className={styles.name}>
-                <div className={styles.formpassword}>
-                  <label htmlFor="name" className={styles.password}>
-                    Prénom de l&apos;hébergeur
-                  </label>
-                  <input
-                    className={styles.textarea}
-                    id="firstname"
-                    type="name"
-                    required
-                  />
-                </div>
-                <div className={styles.formpassword}>
-                  <label htmlFor="name" className={styles.password}>
-                    Nom de l&apos;hébergeur
-                  </label>
-                  <input
-                    className={styles.textarea}
-                    id="secondname"
-                    type="name"
-                    required
-                  />
-                </div>
-              </div>
-              <div className={styles.formpassword}>
-                <label htmlFor="adress" className={styles.password}>
-                  Adresse
-                </label>
-                <input
-                  className={styles.textarea}
-                  id="adress"
-                  type="adress"
-                  required
-                />
-              </div>
-              <div className={styles.name}>
-                <div className={styles.formpassword}>
-                  <label htmlFor="codepostale" className={styles.password}>
-                    Code postal
-                  </label>
-                  <input
-                    className={styles.textarea}
-                    id="codepostal"
-                    type="adress"
-                    required
-                  />
-                </div>
-                <div className={styles.formpassword}>
-                  <label htmlFor="city" className={styles.password}>
-                    Ville
-                  </label>
-                  <input
-                    className={styles.textarea}
-                    id="city"
-                    type="city"
-                    required
-                  />
-                </div>
-              </div>
-              <div className={styles.formpassword}>
-                <label htmlFor="hour" className={styles.password}>
-                  Heure d&apos;arrivée
-                </label>
-                <input
-                  className={styles.textarea}
-                  id="hour"
-                  type="hour"
-                  required
-                />
-              </div>
-              <div className={styles.formpassword}>
-                <label htmlFor="hour" className={styles.password}>
-                  Commentaires
-                </label>
-                <input
-                  className={styles.textarea2}
-                  id="commentaire"
-                  type="commentaire"
-                  required
-                />
-              </div>
-              <div className={styles.formbutton}>
-                <button type="submit" className={styles.button2}>
-                  CONTINUER VERS LE PAIEMENT
-                </button>
-              </div>
-            </form>
+            </div>
+          </>
+        )}
+        <div className={styles.title}>
+          <div className={styles.ligne}>
+            <hr className={styles.hr} />
+          </div>
+          <div className={styles.h2}>
+            <h2 onClick={OpenPayment}>Paiement</h2>
+          </div>
+          <div className={styles.ligne}>
+            <hr className={styles.hr} />
           </div>
         </div>
+        {activePayment && (
+          <>
+            <h3 className={styles.h3}>
+              {" "}
+              <GiPadlock />
+              Choisissez votre moyen de paiement
+            </h3>
+            <div className={styles.paymentparent}>
+              <div className={styles.paymentcontainer}>
+                <div className={styles.checkbox}>
+                  <input
+                    name="subscribe"
+                    type="checkbox"
+                    className={styles.password}
+                  />
+                  <label htmlFor="city" className={styles.password}>
+                    Paypal <BsPaypal />
+                  </label>
+                </div>
+                <div className={styles.checkbox}>
+                  <input
+                    name="subscribe"
+                    type="checkbox"
+                    className={styles.password}
+                  />
+                  <label htmlFor="city" className={styles.password}>
+                    Carte bancaire <FaCcVisa /> <FaCcMastercard />
+                  </label>
+                </div>
+                <div className={styles.checkbox}>
+                  <input
+                    name="subscribe"
+                    type="checkbox"
+                    className={styles.password}
+                  />
+                  <label htmlFor="city" className={styles.password}>
+                    J&apos;accepte les conditions générales de prestations de
+                    services
+                  </label>
+                </div>
+
+                <div className={styles.checkbox}>
+                  <input
+                    name="subscribe"
+                    type="checkbox"
+                    className={styles.password}
+                  />
+                  <label htmlFor="city" className={styles.password}>
+                    J&apos;accepte que l’exécution du service débute dès la
+                    confirmation de la commande.
+                  </label>
+                </div>
+                <div className={styles.formbutton}>
+                  <button
+                    type="submit"
+                    className={styles.button2}
+                    onClick={HandleSubmitPayment}
+                  >
+                    CONFIRMER LA COMMANDE
+                  </button>
+                </div>
+              </div>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
 }
-
-/* <Welcome />
-      <Informations
-        active={active}
-        setActive={setActive}
-        HandleSubmit={HandleSubmit}
-      />
-      {active && <Livraison />} */
