652f21e62cd893c525312089ae47211ee34be949
Claiirou <clairemachet@hotmail.fr>
2022-06-10 12:20:15 +0200

    init db after migrate

--- prisma/migrations/20220610101918_init/migration.sql
+++ prisma/migrations/20220610101918_init/migration.sql
@@ -1,11 +1,14 @@
-/*
-  Warnings:
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NULL,
+    `email` VARCHAR(191) NULL,
+    `hashedPassword` VARCHAR(191) NULL,
+    `role` VARCHAR(191) NOT NULL DEFAULT 'visitor',
 
-  - You are about to drop the `Thing` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-DROP TABLE `Thing`;
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `Customer` (
@@ -16,7 +19,7 @@ CREATE TABLE `Customer` (
     `zip` VARCHAR(10) NOT NULL,
     `city` VARCHAR(60) NOT NULL,
     `email` VARCHAR(50) NOT NULL,
-    `password` VARCHAR(50) NOT NULL,
+    `hashedPassword` VARCHAR(50) NOT NULL,
     `phone` VARCHAR(50) NOT NULL,
 
     PRIMARY KEY (`id`)
