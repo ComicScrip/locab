ae69cd2f657caa8e164f6877f39b680284565a8e
Claiirou <clairemachet@hotmail.fr>
2022-06-16 15:33:21 +0200

    begin css pop up

--- components/AddProductsPopUp.js
+++ components/AddProductsPopUp.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import styles from "../styles/AddProductsPopUp.module.css";
 
-function AddProductsPopUp() {
+function AddProductsPopUp({ show, setShow }) {
   const [name, setName] = useState("");
   const [mark, setMark] = useState("");
   const [quantity, setQuantity] = useState("");
@@ -13,64 +13,81 @@ function AddProductsPopUp() {
   };
 
   return (
-    <div>
-      <h1>Ajouter un produit</h1>
-      <form className={styles.formPopUpAddProducts} onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="nom">Nom</label>
-          <input
-            className={styles.inputPopUp}
-            id="nom"
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          ></input>
-        </div>
-        <div>
-          <label htmlFor="mark">Marque</label>
-          <input
-            className={styles.inputPopUp}
-            id="mark"
-            type="text"
-            value={mark}
-            onChange={(e) => setMark(e.target.value)}
-          ></input>
-        </div>
-        <div>
-          <label htmlFor="quantity">Quantité</label>
-          <input
-            className={styles.inputPopUp}
-            id="quantity"
-            type="text"
-            value={quantity}
-            onChange={(e) => setQuantity(e.target.value)}
-          ></input>
-        </div>
-        <div>
-          <label htmlFor="price">Catégorie de prix</label>
-          <input
-            className={styles.inputPopUp}
-            id="price"
-            type="text"
-            value={price}
-            onChange={(e) => setPrice(e.target.value)}
-          ></input>
-        </div>
-        <div>
-          <label htmlFor="description">Description</label>
-          <textarea
-            className={styles.inputPopUp}
-            id="description"
-            type="textarea"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-          ></textarea>
-        </div>
-        <div>Photos</div>
-        <button type="submit" className={styles.buttonPopUp}>
-          Ajouter
-        </button>
-      </form>
+    <div className={`${styles.popup} ${show ? styles.active : ""} `}>
+      <div className={`${styles.popup__content} ${show ? styles.active : ""}`}>
+        {show && (
+          <span onClick={() => setShow(false)} className={styles.popup__close}>
+            &times;
+          </span>
+        )}
+        <h1>Ajouter un produit</h1>
+        <form className={styles.formPopUpAddProducts} onSubmit={handleSubmit}>
+          <div className={styles.productsName}>
+            <label htmlFor="nom" className={styles.labelPopUp}>
+              Nom
+            </label>
+            <input
+              className={styles.inputPopUp}
+              id="nom"
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            ></input>
+          </div>
+          <div className={styles.productsMark}>
+            <label htmlFor="mark" className={styles.labelPopUp}>
+              Marque
+            </label>
+            <input
+              className={styles.inputPopUp}
+              id="mark"
+              type="text"
+              value={mark}
+              onChange={(e) => setMark(e.target.value)}
+            ></input>
+          </div>
+          <div className={styles.productsQuantity}>
+            <label htmlFor="quantity" className={styles.labelPopUp}>
+              Quantité
+            </label>
+            <input
+              className={styles.inputPopUp}
+              id="quantity"
+              type="text"
+              value={quantity}
+              onChange={(e) => setQuantity(e.target.value)}
+            ></input>
+          </div>
+          <div className={styles.productsPrice}>
+            <label htmlFor="price" className={styles.labelPopUp}>
+              Catégorie de prix
+            </label>
+            <input
+              className={styles.inputPopUp}
+              id="price"
+              type="text"
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+            ></input>
+          </div>
+          <div className={styles.productsDescription}>
+            <label htmlFor="description" className={styles.labelPopUp}>
+              Description
+            </label>
+            <textarea
+              className={styles.inputPopUp}
+              id="description"
+              type="textarea"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            ></textarea>
+          </div>
+          <div>Photos</div>
+          <button type="submit" className={styles.buttonPopUp}>
+            Ajouter
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,11 +1,24 @@
+import { useState } from "react";
 import AddProductsPopUp from "../../components/AddProductsPopUp";
 import LayoutAdmin from "../../components/LayoutAdmin";
 
 export default function Dashboard() {
+  const [showPopup, setShowPopup] = useState(false);
+  const handleClick = () => {
+    setShowPopup(true);
+  };
+
   return (
     <LayoutAdmin pageTitle="Dashboard">
       <h1>Dashboard</h1>
-      <AddProductsPopUp />
+
+      <div>
+        <button onClick={handleClick} type="button">
+          +
+        </button>
+
+        <AddProductsPopUp show={showPopup} setShow={setShowPopup} />
+      </div>
     </LayoutAdmin>
   );
 }
