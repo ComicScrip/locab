cd0c67a80ec49568723e7b772d0ad0b7d89c7e7e
RayaneArroudj <rayane.arroudj@gmail.com>
2022-06-20 16:08:44 +0200

    page moncompte

---
+++ pages/moncompte/index.js
@@ -0,0 +1,11 @@
+import Layout from "../../components/Layout";
+import Banner from "../../components/Banner";
+
+export default function MonCompte() {
+  return (
+    <Layout>
+      <Banner />
+      <h2>Mon compte</h2>
+    </Layout>
+  );
+}
