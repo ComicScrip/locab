82663476adcc998f5a702c01a8cbd93dfe95d8ee
QuentinJOUANNE <QUENTIN.JOUANNE94@gmail.com>
2022-06-23 09:58:31 +0200

    template moncompte page is done, need to join the db

--- pages/moncompte/index.js
+++ pages/moncompte/index.js
@@ -2,59 +2,103 @@ import Layout from "../../components/Layout";
 import Banner from "../../components/Banner";
 import styles from "../../styles/Moncompte.module.css";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-// import { useTranslation } from "next-i18next";
+import { useTranslation } from "next-i18next";
 import { BsArrowLeft } from "react-icons/bs";
+import Link from "next/link";
 
 export default function MonCompte() {
-  // const { t } = useTranslation("moncompte");
+  const { t } = useTranslation("signIn");
 
   return (
     <Layout>
       <Banner />
-      <div className={styles.mainContainerInformationTitle}>
-        <h1>Mes informations personnelles</h1>
+      <div className={styles.titleParaContainerUpdPers}>
+        {" "}
+        <Link href="/admin" title="admin">
+          <a className={styles.linkParaStyle}>
+            <BsArrowLeft
+              style={{
+                verticalAlign: "middle",
+              }}
+            />{" "}
+            {t("retourcompte")}
+          </a>
+        </Link>
+        <h1 className={styles.titleUpd}>{t("mesinfoperso")}</h1>
       </div>
-      <div className={styles.monCptContainerPara}>
-        <BsArrowLeft />
-        <p>retour au compte</p>
-      </div>
-      <div className={styles.mainInputContainer}>
-        <div className={styles.inputContainer}>
+
+      <div className={styles.updRegisterForm}>
+        <form>
+          <div className={styles.inpNameLastName}>
+            <label className={styles.labelStyle}>
+              {t("prenom")}
+              <input
+                className={styles.inputUpdatePersInfo}
+                type="text"
+                id="firstName"
+              />
+            </label>
+            <label className={styles.labelStyle}>
+              {t("nom")}
+              <input
+                className={styles.inputUpdatePersInfo}
+                type="text"
+                id="name"
+              />
+            </label>
+          </div>
           <label className={styles.labelStyle}>
-            Prénom
-            <input type="text" className={styles.inputCptStyle} />
+            {t("adresse")}
+            <input
+              className={styles.bigInputUpdPersInfo}
+              type="text"
+              id="adresse"
+            />
           </label>
-          <label className={styles.labelStyle}>
-            Nom
-            <input type="text" className={styles.inputCptStyle} />
-          </label>
-        </div>
-        <div className={styles.testC}>
-          <label className={styles.labelStyle}>
-            Adresse
-            <input className={styles.inputCptThird} type="text" />
-          </label>
-        </div>
-        <div className={styles.inputContainer}>
-          <label className={styles.labelStyle}>
-            Code postal
-            <input type="text" />
-          </label>
-          <label className={styles.labelStyle}>
-            Ville
-            <input type="text" />
-          </label>
-        </div>
-        <div className={styles.inputContainer}>
-          <label className={styles.labelStyle}>
-            Numéro de téléphone
-            <input type="text" />
-          </label>
-          <label className={styles.labelStyle}>
-            Adresse mail
-            <input type="mail" />
-          </label>
-        </div>
+          <div className={styles.codeVille}>
+            <label className={styles.labelStyle}>
+              {t("cp")}
+              <input
+                className={styles.inputUpdatePersInfo}
+                type="text"
+                id="codePostal"
+              />
+            </label>
+            <label className={styles.labelStyle}>
+              {t("ville")}
+              <input
+                className={styles.inputUpdatePersInfo}
+                type="text"
+                id="ville"
+              />
+            </label>
+          </div>
+          <div className={styles.codeVille}>
+            <label className={styles.labelStyle}>
+              {t("Numérodetelephone")}
+              <input
+                className={styles.bigInputUpdPersInfo}
+                type="text"
+                id="telephone"
+              />
+            </label>
+            <label className={styles.labelStyle}>
+              {t("adressemail")}
+              <input
+                className={styles.bigInputUpdPersInfo}
+                type="email"
+                id="email"
+              />
+            </label>
+          </div>
+          <button
+            className={styles.btnValidateUpd}
+            type="submit"
+            id="validate-btn"
+          >
+            Valider
+          </button>
+        </form>
       </div>
     </Layout>
   );
