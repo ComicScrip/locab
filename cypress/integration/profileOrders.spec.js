043d17f6b4af888610113af384221f6eea293c6c
JVRasa <j.rasamoelina@gmail.com>
2022-06-28 10:36:14 +0200

    added tests for filter

--- cypress/integration/profileOrders.spec.js
+++ cypress/integration/profileOrders.spec.js
@@ -1,20 +1,30 @@
 describe("orders - without session", () => {
   it("asks to log in if the user isn't signed in", () => {
-    cy.task("resetDB");
     cy.visit("/profile/orders");
     cy.contains("Veuillez vous connecter pour accéder à votre compte");
   });
 });
 
 describe("orders - with an active session", () => {
-  Cypress.on("uncaught:exception", () => {
-    return false;
+  beforeEach(() => {
+    cy.task("resetDB");
+    cy.task("createOrderSample");
+    cy.loginProfile({ email: "tata@locab.com", password: "locablocab" });
+    cy.visit("/profile/orders");
   });
 
   it("can access the profile page if the user is signed in", () => {
-    cy.task("resetDB");
-    cy.setupCurrentUser({ email: "toto@alaplage.com" });
-    cy.visit("/profile/orders");
     cy.contains("Mes commandes");
+  });
+
+  it.only("order - shows all my orders", () => {
+    cy.contains("ART123");
+  });
+
+  it.only("order - shows only the orders made according to the selected filter", () => {
+    cy.get('[data-cy="dateSelect"]').select("last3months");
+    cy.contains("A54363");
+    cy.get('[data-cy="dateSelect"]').select("lastmonth");
+    cy.contains("A366UL");
   });
 });
