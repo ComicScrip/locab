0d2da7ade7b077edfdac7114c18e53bf4408fed9
Claiirou <clairemachet@hotmail.fr>
2022-07-07 11:44:28 +0200

    create model and road for price category

--- pages/api/priceCategory/index.js
+++ pages/api/priceCategory/index.js
@@ -1,8 +1,62 @@
 import base from "../../../middlewares/common";
-import { getPriceCategory } from "../../../models/priceCategory";
+import {
+  getPriceCategory,
+  createPrice,
+  searchPrice,
+} from "../../../models/priceCategory";
 
 async function handleGetPriceCatgory(req, res) {
   res.send(await getPriceCategory());
 }
 
-export default base().get(handleGetPriceCatgory);
+async function handlePostPrice(req, res) {
+  const {
+    name,
+    oneDay,
+    twoDays,
+    threeDays,
+    fourDays,
+    fiveDays,
+    sixDays,
+    sevenDays,
+    eightDays,
+    nineDays,
+    tenDays,
+    elevenDays,
+    twelveDays,
+    thirteenDays,
+    fourteenDays,
+    fifteenDays,
+    sixteenDays,
+  } = req.body;
+  return res.status(201).send(
+    await createPrice({
+      name,
+      oneDay,
+      twoDays,
+      threeDays,
+      fourDays,
+      fiveDays,
+      sixDays,
+      sevenDays,
+      eightDays,
+      nineDays,
+      tenDays,
+      elevenDays,
+      twelveDays,
+      thirteenDays,
+      fourteenDays,
+      fifteenDays,
+      sixteenDays,
+    })
+  );
+}
+
+async function handleGetPrice(req, res) {
+  res.send(await searchPrice({ search: req.query.search }));
+}
+
+export default base()
+  .get(handleGetPriceCatgory)
+  .post(handlePostPrice)
+  .get(handleGetPrice);
